0d7f6b0f7b4bf0672fbd58e873f143af7d34a000
damirpie <damirdamir692@gmail.com>
2024-01-17 22:40:26 +0600

    1:58:00 | cart page

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import {Cart, CartModel} from "../../models/cart.model";
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: 'cart.component.html',
+  styles: [
+  ]
+})
+export class CartComponent implements OnInit {
+
+  cart: Cart = { items: [
+      {
+          id: 1,
+          product: 'https://via.placeholder.com/150',
+          name: 'snickers',
+          price: 150,
+          quantity: 1
+      },
+          {
+              id: 2,
+              product: 'https://via.placeholder.com/150',
+              name: 'snickers',
+              price: 150,
+              quantity: 2
+          }
+    ]
+  }
+
+  dataSource: Array<CartModel> = []
+  displayedColumns: Array<string> = [
+      'product',
+      'name',
+      'price',
+      'quantity',
+      'total',
+      'action'
+  ]
+  constructor() { }
+
+  ngOnInit(): void
+  {
+    this.dataSource = this.cart.items
+  }
+
+    getTotal(items: Array<CartModel>):number {
+        return items.map((item) => item.price * item.quantity).reduce((prev, current) => prev + current, 0)
+    }
+}
